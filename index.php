e862198c239bb4e79fd1d062bdc8e8e7bf07d707
Peta <peter840628@gmail.com>
2017-05-08 08:49:19 -0700

    pages creation
    login page, register page, profile page, log out page and all the
    database connection and validation

---
+++ index.php
@@ -0,0 +1,7 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<form name="form" method="post" action="connect.php">
+    Account：<input type="text" name="id" /> <br>
+    Password：<input type="password" name="pw" /> <br>
+    <input type="submit" name="button" value="Login" />&nbsp;&nbsp;
+    <a href="register.php">Register</a>
+</form>
