197276c5890fef7d25239a84497f0a97dc28d40c
Minitte <arcentalminitte@gmail.com>
2017-05-08 22:20:33 -0700

    SQL statement ensures one row
    Added `LIMIT 1`

--- php/member.php
+++ php/member.php
@@ -11,20 +11,20 @@ if($_SESSION['username'] != null)
         echo '<a href="updateprof.php">edit</a>  <br><br>  ';
     
         //showing the profile
-        $sql = "SELECT * FROM member_table WHERE username = '$user' ";
+        $sql = "SELECT * FROM member_table WHERE username = '$user' LIMIT 1";
         $result = mysql_query($sql);
         while($row = mysql_fetch_row($result))
         {
-                 echo "
-                 name(account)：$row[1]<br> " .
-                 "likes：$row[3]<br>
-                 dislike：$row[4]<br> 
-                 allergy：$row[5]<br>";
+             echo "
+             name(account)：$row[1]<br> " .
+             "likes：$row[3]<br>
+             dislike：$row[4]<br> 
+             allergy：$row[5]<br>";
         }
 }
 else
 {
-        echo 'oops! some error just happened';
-        echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
+    echo 'oops! some error just happened';
+    echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
 }
 ?>
