ced2fe0b28e164ed85c43942a8f0d15672101491
Anthony <awong6201@gmail.com>
2017-05-10 09:59:47 -0700

    logout css link hotfix

--- logout.php
+++ logout.php
@@ -17,7 +17,7 @@ echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         
-        <link rel="stylesheet" href="logout.css">
+        <link rel="stylesheet" href="css/logout.css">
     </head>
     <body>
         <header>
