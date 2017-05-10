ef0200da073f905612486f3826a40d316a2e23b7
Si-Yang Wu <siyang7@hotmail.com>
2017-05-10 11:06:37 -0700

    Changed styling for registration success and failure page

--- register_finish.php
+++ register_finish.php
@@ -10,6 +10,26 @@ $likes = $_POST['likes'];
 $dislikes = $_POST['dislikes'];
 $allergies = $_POST['allergies'];
 
+echo '<title>Registeration Success</title> 
+    <meta charset="utf-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/styles.css">';
+
+echo '<header>
+        <div class="container">
+            <div id="logo" class="container">
+                <div class="col-xs-12">
+                    <img src="https://raw.githubusercontent.com/ZhiyuanEric/2910MyFoodList/Login-Page/images/logo.png">
+                    <h1 class="title"> FoodDoc </h1>
+                </div>
+            </div>
+        </div>
+    </header>';
 
 if($id != null && $pw != null && $pw2 != null && $pw == $pw2)//checking is there empty field and is the password the same
 {
@@ -17,18 +37,32 @@ if($id != null && $pw != null && $pw2 != null && $pw == $pw2)//checking is there
         $sql = "insert into member_table (username, password, likes, dislikes, allergies) values ('$id', '$pw', '$likes', '$dislikes', '$allergies')";
         if(mysql_query($sql))
         {
-                echo 'Registering succeed!';
+                echo '<main>
+                        <div class="register">
+                            Registration Success!
+                        </div> 
+                      </main>';
                 echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
         }
         else
         {
-                echo 'Registering fail!';
+                echo '<main>
+                          <div class="failed">
+                              Registration failed! Account already exist!
+                          </div>
+                      </main>';
                 echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
         }
 }
 else
 {
-        echo 'the passwords are different or there is empty field!';
+        echo '<main>
+                  <div class="failed">
+                      Registration failed. The passwords are different 
+                      and there are empty fields.
+                  </div>
+              </main>';
+
         echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
 }
 ?>
