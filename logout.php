f4d1ae6a83f4c85fa7f75bfe12d82d71058326c3
Anthony <awong6201@gmail.com>
2017-05-15 12:10:38 -0700

    include files
    finishing up
    fixed up the register_finish html

--- logout.php
+++ logout.php
@@ -8,35 +8,17 @@ echo '<meta http-equiv=REFRESH CONTENT=2;url=index.php>';
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>FoodDocs - Logged out</title>
-        <meta http-equiv="Content-Type" content="text/html"; charset=utf-8/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        
-		<link rel="stylesheet" href="css/logout.css">
-        <link rel="stylesheet" href="css/header.css">
-		<link rel="stylesheet" href="css/style.css">
-    </head>
+    <?php include("include/head.inc"); ?>
     <body>
-        <header>
-            <div id="logo" class="container">
-                <div class="col-xs-12">
-                    <img src="images/logo.png">
-                </div>
-            </div>
-            <div>
-                <div class="navbar navbar-default">
-                    <div class="container-fluid">
-                        <ul class="nav navbar-nav">
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </header>
+		<?php include("include/logged_out_header.inc"); ?>
+        <div>
+			<div class="navbar navbar-default">
+				<div class="container-fluid">
+					<ul class="nav navbar-nav">
+					</ul>
+				</div>
+			</div>
+        </div>
         <main class="container">
             <div class="text-center msgBlock">
                 <h2> Logged out</h2>
